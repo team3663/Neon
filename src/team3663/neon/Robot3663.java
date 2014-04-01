5db42622148eec579ae14e3dbf852b3a6ad13d7c
CurtisAndersen <curtis@loginet.com>
2014-03-31 17:55:36 -0700

    code that is running on the robot as of this moment

--- src/team3663/neon/Robot3663.java
+++ src/team3663/neon/Robot3663.java
@@ -21,7 +21,7 @@ public class Robot3663 extends IterativeRobot
     CommandGroup autonomousCG;
     CommandGroup lossenAndWindWinch;
     //CommandBase c_Drive;
-    //SendableChooser autoChooser;
+    SendableChooser autoChooser;
 
     DriverStation driveStation;
     int isAliveCounter;
@@ -50,6 +50,7 @@ public class Robot3663 extends IterativeRobot
         driveStation.getBatteryVoltage();
         CommandBase.dsLCD.clear();
         updateStatusNextRefresh = Timer.getFPGATimestamp();
+        clearSmartDashboard();
         updateStatus();
         System.out.println("Robot3663.robotInit end");
     }
@@ -108,6 +109,58 @@ public class Robot3663 extends IterativeRobot
     {
         //System.out.println("disablePeriodic.start");
         //System.out.println("disablePeriodic.end");
+    }    
+    public void clearSmartDashboard()
+    {
+        SmartDashboard.putString("CG_AutonomousComplete", "-reset-");
+        SmartDashboard.putString("CG_AutonomousMoveAndShoot", "-reset-");
+        SmartDashboard.putString("CG_AutonomousVisionOnly", "-reset-");
+        SmartDashboard.putString("CG_FireAfterBackUp", "-reset-");
+        SmartDashboard.putString("CG_FireWithArmUp", "-reset-");
+        SmartDashboard.putString("CG_HammerFire", "-reset-");
+        SmartDashboard.putString("CG_HammerFireAfterDriveForward", "-reset-");
+        SmartDashboard.putString("CG_ShootAndRecock", "-reset-");
+        SmartDashboard.putString("CG_TestAllPartsOfTheRobot", "-reset-");
+        SmartDashboard.putString("CG_TractionWheelsDownLowGear", "-reset-");
+        SmartDashboard.putString("CG_TractionWheelsUpHighGear", "-reset-");
+        SmartDashboard.putString("CG_WindAndLatchToFullPower", "-reset-");
+        SmartDashboard.putString("C_CatapultLimitSwitchMonitor", "-reset-");
+        SmartDashboard.putString("C_Drive", "-reset-");
+        SmartDashboard.putString("C_DriveBasedOnEncoders", "-reset-");
+        SmartDashboard.putString("C_DriveForwardTime", "-reset-");        
+        SmartDashboard.putString("C_FillAirTanks", "-reset-");     
+        SmartDashboard.putString("C_LaunchBall", "-reset-");        
+        SmartDashboard.putString("C_LoadBall", "-reset-");        
+        SmartDashboard.putString("C_LoosenWinchAndLatch", "-reset-");      
+        SmartDashboard.putString("C_TimeWait", "-reset- ");        
+        SmartDashboard.putString("C_WaitForCompressor", "-reset-");        
+        SmartDashboard.putString("C_WindWinch", "-reset- ");
+        SmartDashboard.putString("P_ArmDown", "-reset-");        
+        SmartDashboard.putString("P_ArmUp", "-reset-");        
+        SmartDashboard.putString("P_FootDown", "-reset-");        
+        SmartDashboard.putString("P_FootUp", "-reset-");        
+        SmartDashboard.putString("P_HammerExtend", "-reset-");        
+        SmartDashboard.putString("P_HammerRetract", "-reset-");        
+        SmartDashboard.putString("P_LatchClose", "-reset-");        
+        SmartDashboard.putString("P_LatchOpen", "-reset-");        
+        SmartDashboard.putString("P_ShiftToHighGear", "-reset-");        
+        SmartDashboard.putString("P_ShiftToLowGear", "-reset-");        
+        SmartDashboard.putString("P_SpinArmMotor", "-reset-");
+        SmartDashboard.putString("P_TractionWheelsDown", "-reset-");        
+        SmartDashboard.putString("P_TractionWheelsUp", "-reset-");        
+        SmartDashboard.putString("P_WinchLoosen", "-reset-");        
+        SmartDashboard.putString("P_WinchTighten", "-reset-");        
+        SmartDashboard.putString("Arm", "-reset-");
+        SmartDashboard.putString("Catapult", "-reset-");
+        SmartDashboard.putString("Air tanks", "-reset-");
+        SmartDashboard.putString("Compressor", "-reset-");
+        SmartDashboard.putString("Gear","-reset-");
+        SmartDashboard.putString("Traction wheels","-reset-");
+        SmartDashboard.putString("Driving mode", "-reset-");
+        SmartDashboard.putString("Foot","-reset-");
+        SmartDashboard.putString("Hammer","-reset-");
+        SmartDashboard.putString("Goal", "-reset-");
+        SmartDashboard.putString("Latch","-reset-");
     }
     public void updateStatus()
     {
@@ -138,6 +191,22 @@ public class Robot3663 extends IterativeRobot
             //CommandBase.dsLCD.println(DriverStationLCD.Line.kUser4, 1, "                     ");
             CommandBase.dsLCD.println(DriverStationLCD.Line.kUser5, 1, "                     ");
             CommandBase.dsLCD.println(DriverStationLCD.Line.kUser6, 1, "                     ");
+            if(isOperatorControl())
+            {
+                SmartDashboard.putString("RobotState", "Teleoperated");
+            }
+            else if(isAutonomous())
+            {
+                SmartDashboard.putString("RobotState", "Autonomous");
+            }
+            else if(isTest())
+            {
+                SmartDashboard.putString("RobotState", "Test");
+            }
+            else
+            {
+                SmartDashboard.putString("RobotState", "Practice");
+            }
         }
     }
 }
